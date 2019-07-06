86ebdc98eb8391a7dfcb8c503ec4be05d57cbee3
Aashish Karki <aashish.y2z@gmail.com>
2019-07-06 22:30:37 +0545

    updates

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import TypeAhead from 'typeahead-lib';
+import './App.css';
+
 
 function App() {
   return (
-    <div />
+    <div style={{ margin: '100px', width: '600px' }}>
+      <TypeAhead options={['Apple', 'Aeroplane', 'Ball', 'Basker']} />
+    </div>
   );
 }
 
